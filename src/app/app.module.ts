a293d1ce0aa6c5300a6eef5eaf76edd7469f474d
Mingxin Yu <mingxinyu@mingxins-mbp.lan>
2020-05-21 21:57:31 -0400

    second revise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RoomsComponent } from './rooms/rooms.component';
-import { RoomsDetailComponent } from './rooms-detail/rooms-detail.component';
-//import { FurnituresComponent } from './furnitures/furnitures.component';
-//import { FurnituresDetailComponent } from './furnitures-detail/furnitures-detail.component';
-//import { MessagesComponent } from './messages/messages.component';
-
-//import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-//import { InMemoryDataService } from './in-memory-data.service';
+import { FurnituresComponent } from './furnitures/furnitures.component';
+import { FurnituresDetailComponent } from './furnitures-detail/furnitures-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoomsComponent,
-    RoomsDetailComponent,
     FurnituresComponent,
     FurnituresDetailComponent,
     MessagesComponent
+
   ],
   imports: [
     BrowserModule,
@@ -32,6 +27,7 @@ import { RoomsDetailComponent } from './rooms-detail/rooms-detail.component';
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
